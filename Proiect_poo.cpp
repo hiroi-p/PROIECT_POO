4fc3b4e0ff526dc6b3247d449bf72c9cf6c0b2f4
hiroi-p <nadaseanpaul@gmail.com>
2024-01-11 19:55:49 +0000

    modified:   Carti.txt 	new file:   Date_Utilizatori.txt 	new file:   Gigel_Frone.txt 	modified:   Proiect_poo.cpp 	modified:   Proiect_poo.o 	modified:   Proiect_poo_2.o

--- Proiect_poo.cpp
+++ Proiect_poo.cpp
@@ -6,33 +6,86 @@
 using namespace std;
 // ce simplu era daca foloseam listele de la inceput <3 <3
 void Meniu_conectare();
-void Meniu_Autentificat();
+void Meniu_Autentificat(string fisier);
 void Meniu_principal();
 void Meniu_contact();
+void Meniu_contact(string fisier);
 void Meniu_Rezervare();
 void Meniu_Istoric();
 void Meniu_Autentificare();
 void Meniu_Inregistrare();
+void Meniu_Rezervare(string fisier);
 // Optiune folosim toate datele unui utilizator pe o linie(si impartim cu strtok).
 // Putem sa folosim un delimitator la finalul istoricului, ex:linie,spatiu ca sa delimitam userii
-class Istoric
+// Putem sa numim in fisier inainte de nume sa scriem Nume: inainte de prenume sa scriem Prenume: si asa mai departe
+int carti_stoc = 0;
+class Carte
 {
 private:
-    list<string> *tranzactii;
-    int suma_totala;
+    string nume;
+    list<string> autori;
+    string editura;
+    string stare;
 
 public:
-    void adaugare(string tranzactie, int suma)
+    Carte(string nume, string editura)
     {
-        tranzactii->push_back(tranzactie);
-        suma_totala = suma_totala + suma;
+        this->nume = nume;
+        this->editura = editura;
+    }
+
+    virtual int pret() = 0;
+};
+
+class Carte_fictiune : private Carte
+{
+    int nr_pag;
+
+public:
+    Carte_fictiune(string nume, string editura, int nr_pag) : Carte(nume, editura)
+    {
+        this->nr_pag = nr_pag;
+    }
+    int pret() override
+    {
+        return 50000 + nr_pag;
+    }
+};
+class Carte_non_fictiune : private Carte
+{
+    int an;
+
+public:
+    Carte_non_fictiune(string nume, string editura, int an) : Carte(nume, editura)
+    {
+        this->an = an;
+    }
+    int pret() override
+    {
+        return 50000 - 25 * an;
+    }
+};
+
+class Tranzactie
+{
+private:
+    string carte;         // sau carte- obiect
+    string tip_achizitie; // aici am modificat
+    int suma;
+
+public:
+    Tranzactie(string carte, string tip_achizitie, int suma)
+    {
+        this->carte = carte;
+        this->tip_achizitie = tip_achizitie;
+        this->suma = suma;
     }
 };
 class User
 {
 private:
     string nume, prenume, CNP, email, parola;
-    Istoric *ist;
+    list<Tranzactie *> istoric;
 
 public:
     User(string nume, string prenume, string CNP, string email, string parola)
@@ -42,11 +95,14 @@ public:
         this->nume = nume;
         this->parola = parola;
         this->prenume = prenume;
-        ist = new Istoric;
     }
-    void adaugaree(string tranzactie, int suma)
+    void adaugaree(string carte, string tip_achizitie, int suma)
     {
-        ist->adaugare(tranzactie, suma);
+        fstream f;
+        f.open(nume + "_" + prenume, ios::out);
+        Tranzactie *isto;
+        isto = new Tranzactie(carte, tip_achizitie, suma);
+        istoric.push_back(isto);
     }
     void afisare()
     {
@@ -72,12 +128,21 @@ public:
              << parola << endl;
         fout << "Istoric:" << endl;
     }
+    // functia de autentificare se poate face aici
+    // functia de resetare parola se poate face aici, precum toate celelalte functii care au legatura cu continutul fisierului
 };
 fstream &operator>>(fstream &fin, User *u)
 {
     string nume, prenume, CNP, email, parola;
-    fin >> nume >> prenume;
+    fin >> nume >> prenume >> CNP >> email >> parola;
+    getline(fin, nume);
+    getline(fin, prenume);
+    getline(fin, CNP);
+    getline(fin, email);
+    getline(fin, parola);
+
     u = new User(nume, prenume, CNP, email, parola);
+
     return fin;
 }
 fstream &operator<<(fstream &fout, User *u)
@@ -98,12 +163,14 @@ void citire_lista(list<User *> &lista_Utilizatori)
 }
 void Meniu_Inregistrare()
 {
+    fstream f;
     User *u;
     string nume, prenume, CNP, email, parola;
     cout << "Nume: ";
     cin >> nume;
     cout << "Prenume: ";
     cin >> prenume;
+    f.open(nume + "_" + prenume + ".txt", ios::out);
     cout << "CNP: ";
     cin >> CNP;
 eticheta:
@@ -112,7 +179,7 @@ eticheta:
         cout << "Email: ";
         cin >> email;
         regex emailRegex(R"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})");
-        if (regex_match(email, emailRegex) == false)
+        if (!regex_match(email, emailRegex))
         {
             throw logic_error("Adresa de email nu respecta formatul.");
         }
@@ -147,36 +214,45 @@ etiketa:
         goto etiketa;
     }
     u = new User(nume, prenume, CNP, email, parola);
-    fstream f;
-    f.open("Date_Utilizatori.txt", ios::app);
     f << u;
     f.close();
+    fstream fi("Date_Utilizatori.txt", ios::app);
+    fi << email << endl
+       << nume + "_" + prenume << endl;
+    fi.close();
+    Meniu_Autentificat(nume + "_" + prenume + ".txt");
 }
 void Meniu_Autentificare()
 {
+    int ok = 0;
     string email, parola, linie;
-    ifstream f("Date_Utilizatori.txt");
+    ifstream f;
+    ifstream fi;
+    fstream flog;
+
 eticheta:
+
     try
     {
         cout << "Email: ";
         cin >> email;
         regex emailRegex(R"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})");
-        if (regex_match(email, emailRegex) == false)
+        if (!regex_match(email, emailRegex))
         {
             throw logic_error("Adresa de email nu respecta formatul.");
         }
     }
     catch (logic_error &e)
     {
-        fstream flog;
         flog.open("log.txt", ios::app);
         flog << e.what() << endl;
         cout << e.what() << endl;
         flog.close();
         goto eticheta;
     }
+
 etiketa:
+
     try
     {
 
@@ -189,26 +265,134 @@ etiketa:
     }
     catch (logic_error &e)
     {
-        fstream flog;
         flog.open("log.txt", ios::app);
         flog << e.what() << endl;
         cout << e.what() << endl;
         flog.close();
         goto etiketa;
     }
+    f.open("Date_Utilizatori.txt");
     while (getline(f, linie))
     {
+        ok = 0;
         regex emailRegex(R"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})");
-        if (regex_match(linie, emailRegex) == true)
+        if (regex_match(linie, emailRegex))
         {
             if (linie == email)
             {
+                ok = 1;
                 getline(f, linie);
-                if (linie == parola)
+                fi.open(linie + ".txt");
+                string nume_fisier;
+                nume_fisier = linie + ".txt";
+                for (int i = 1; i <= 5; i++)
                 {
+                    getline(fi, linie);
+                }
+                if (parola == linie)
+                {
+                    cout << "Autentificare cu succes" << endl;
+                    Meniu_Autentificat(nume_fisier);
+                }
+                else
+                {
+                    cout << "Date de autentificare eronate." << endl;
+                    flog.open("log.txt", ios::app);
+                    flog << "Date de autentificare eronate." << endl;
+                    flog.close();
+                    fi.close();
+                    f.close();
+                    goto eticheta;
                 }
             }
         }
+    }
+    if (ok == 0)
+    {
+        flog.open("log.txt", ios::app);
+        flog << "Date de autentificare eronate." << endl;
+        cout << "Date de autentificare eronate" << endl;
+        flog.close();
+        f.close();
+        goto eticheta;
+    }
+}
+
+void Resetare_Parola()
+{
+    int ok = 0;
+    string email, parola, linie, linie2;
+    ifstream f;
+    fstream fi;
+    fstream flog;
+
+eticheta:
+
+    try
+    {
+        cout << "Email: ";
+        cin >> email;
+        regex emailRegex(R"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})");
+        if (!regex_match(email, emailRegex))
+        {
+            throw logic_error("Adresa de email nu respecta formatul.");
+        }
+    }
+    catch (logic_error &e)
+    {
+        flog.open("log.txt", ios::app);
+        flog << e.what() << endl;
+        cout << e.what() << endl;
+        flog.close();
+        goto eticheta;
+    }
+    f.open("Date_Utilizatori.txt");
+    while (getline(f, linie))
+    {
+        ok = 0;
+        regex emailRegex(R"([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})");
+        if (regex_match(linie, emailRegex))
+        {
+            if (linie == email)
+            {
+                ok = 1;
+                string parola_noua;
+                cout << "Parola noua este:";
+                cin >> parola_noua;
+                getline(f, linie);
+                fi.open(linie + ".txt", ios::in);
+                list<string> linii;
+                while (getline(fi, linie2))
+                    linii.push_back(linie2);
+                fi.close();
+                fi.open(linie + ".txt", ios::out);
+                int aux = 1;
+                for (auto i : linii)
+                {
+                    if (aux == 5)
+                    {
+                        fi << parola_noua << endl;
+                    }
+                    else
+                    {
+                        fi << i << endl;
+                    }
+                    aux++;
+                }
+                cout << "Parola a fost modificata" << endl;
+                Meniu_Autentificat(linie + ".txt");
+                break;
+            }
+        }
+    }
+    if (ok == 0)
+    {
+        flog.open("log.txt", ios::app);
+        flog << "Email inexistent" << endl;
+        cout << "Email inexistent" << endl;
+        flog.close();
+        f.close();
+        goto eticheta;
     }
 }
 void Meniu_conectare()
@@ -227,8 +411,10 @@ void Meniu_conectare()
         Meniu_Inregistrare();
         break;
     case 2:
+        Meniu_Autentificare();
         break;
     case 3:
+        Resetare_Parola();
         break;
     case 4:
         Meniu_principal();
@@ -244,9 +430,9 @@ void Meniu_contact()
     while (1)
     {
         cout << "Suport tehnic" << endl
-             << "In cazul în care întâlniți dificultăți echipa de suport vă va sta la dispoziție." << endl
-             << "Tot ce trebuie să faceți este să completați următorul formular în care trebuie descrisă problema." << endl
-             << "Alegeți o opțiune:" << endl
+             << "In cazul in care intalnit dificultati echipa de suport va va sta la dispozitie." << endl
+             << "Tot ce trebuie să faceti este sa completati urmatorul formular in care trebuie descrisa problema." << endl
+             << "Alegeti o optiune:" << endl
              << "     1. Trimitere sesizare" << endl
              << "     2. Inapoi" << endl
              << "Optiune: ";
@@ -256,6 +442,7 @@ void Meniu_contact()
         case 1:
             break;
         case 2:
+
             Meniu_principal();
             break;
         default:
@@ -263,12 +450,24 @@ void Meniu_contact()
         }
     }
 }
-void Meniu_Autentificat(string email, string parola)
+void Meniu_Autentificat(string fisier)
 {
+    fstream f;
+    string linie;
     int opt;
+    int aux = 1;
+    f.open(fisier, ios::in);
+    getline(f, linie);
+    cout << "Bookstore App"
+         << " " << linie << " ";
+    getline(f, linie);
+    cout << linie << endl;
+    f.close();
+    int suma_totala = 0;
     while (1)
     {
-        cout << "1.Achizitie/Imprumut" << endl;
+        aux = 1;
+        cout << "1.Rezervare" << endl;
         cout << "2.Istoric" << endl;
         cout << "3.Contact" << endl;
         cout << "Optiune: ";
@@ -276,11 +475,179 @@ void Meniu_Autentificat(string email, string parola)
         switch (opt)
         {
         case 1:
+            Meniu_Rezervare(fisier);
             break;
         case 2:
+            suma_totala = 0;
+            f.open(fisier, ios::in);
+            while (getline(f, linie))
+            {
+                if (aux >= 6)
+                {
+                    if (aux % 5 == 0)
+                    {
+                        suma_totala = suma_totala + stoi(linie);
+                    }
+                    cout << linie << endl;
+                }
+                aux++;
+            }
+            cout << "Suma totala cheltuita:" << suma_totala << endl;
+            f.close();
             break;
-
+        case 3:
+            Meniu_contact(fisier);
+            break;
         default:
+            cout << "optiune gresita" << endl;
+            break;
+        }
+    }
+}
+void Meniu_Rezervare(string fisier)
+{
+    fstream f;
+    string linie, nume, linie2;
+    int opt;
+    int aux = 1;
+    f.open(fisier, ios::in);
+    getline(f, linie);
+    cout << "Bookstore App"
+         << " " << linie << " ";
+    getline(f, linie);
+    cout << linie << endl;
+    f.close();
+    cout << "Cartile sunt:" << endl;
+    fstream fi("Carti.txt", ios::in);
+    while (getline(fi, linie))
+    {
+        cout << linie << endl;
+    }
+    fi.close();
+    while (1)
+    {
+        int opt;
+        cout << "1.Achizitie" << endl;
+        cout << "2.Imprumut" << endl;
+        cout << "3.Inapoi" << endl;
+        cin >> opt;
+
+        switch (opt)
+        {
+        case 1:
+            cout << "Numele cartii dorite:";
+            cin.ignore();
+            getline(cin, nume);
+            fi.open("Carti.txt", ios::in);
+            while (getline(fi, linie))
+            {
+                if (linie == nume)
+                {
+                    getline(fi, linie);
+                    fi.close();
+                    if (linie == "Disponibil")
+                    {
+                        cout << "Carte achizitionata" << endl;
+                        fi.open("Carti.txt", ios::in);
+                        list<string> linii;
+                        while (getline(fi, linie))
+                            linii.push_back(linie);
+                        fi.close();
+                        fi.open("Carti.txt", ios::out);
+                        auto i = find(linii.begin(), linii.end(), nume);
+                        *next(i) = "Vandut";
+                        for (auto i : linii)
+                            fi << i << endl;
+                        carti_stoc--;
+                        fi.close();
+                        f.open(fisier, ios::app);
+                        fi.open("Carti.txt", ios::in);
+                        while (getline(fi, linie))
+                        {
+                            if (linie == nume)
+                            {
+
+                                for (int i = 1; i <= 6; i++)
+                                {
+                                    if (i != 2)
+                                        f << linie << endl;
+                                    getline(fi, linie);
+                                }
+
+                                f.close();
+                                fi.close();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        cout << "Carte indisponibila" << endl;
+                    }
+                }
+            }
+            Meniu_Autentificat(fisier);
+            break;
+        case 2:
+            cout << "Numele cartii dorite:";
+            cin.ignore();
+            getline(cin, nume);
+            fi.open("Carti.txt", ios::in);
+            while (getline(fi, linie))
+            {
+                if (linie == nume)
+                {
+                    getline(fi, linie);
+                    fi.close();
+                    if (linie == "Disponibil")
+                    {
+                        cout << "Carte imprumutata" << endl;
+                        fi.open("Carti.txt", ios::in);
+                        list<string> linii;
+                        while (getline(fi, linie))
+                            linii.push_back(linie);
+                        fi.close();
+                        fi.open("Carti.txt", ios::out);
+                        auto i = find(linii.begin(), linii.end(), nume);
+                        *next(i) = "Imprumutat";
+                        for (auto i : linii)
+                            fi << i << endl;
+                        fi.close();
+                        f.open(fisier, ios::app);
+                        fi.open("Carti.txt", ios::in);
+                        while (getline(fi, linie))
+                        {
+                            if (linie == nume)
+                            {
+
+                                for (int i = 1; i <= 6; i++)
+                                {
+                                    if (i != 2)
+                                        if (i == 5)
+                                            f << "0" << endl;
+                                        else
+                                            f << linie << endl;
+                                    getline(fi, linie);
+                                }
+
+                                f.close();
+                                fi.close();
+                            }
+                        }
+                    }
+                    else
+                    {
+                        cout << "Carte indisponibila" << endl;
+                    }
+                }
+            }
+
+            Meniu_Autentificat(fisier);
+            break;
+        case 3:
+            Meniu_Autentificat(fisier);
+            break;
+        default:
+            cout << "Optiune Gresita!";
             break;
         }
     }
@@ -303,6 +670,7 @@ void Meniu_principal()
         Meniu_conectare();
         break;
     case 2:
+
         Meniu_contact();
         break;
     default:
@@ -311,12 +679,34 @@ void Meniu_principal()
         break;
     }
 }
-int main()
+void Meniu_contact(string fisier)
 {
     int opt;
-    list<User *> lista_Utilizatori;
-    string chestie = "adasdasdwq";
-    ofstream f(chestie);
+    while (1)
+    {
+        cout << "Suport tehnic" << endl
+             << "In cazul in care intalnit dificultati echipa de suport va va sta la dispozitie." << endl
+             << "Tot ce trebuie să faceti este sa completati urmatorul formular in care trebuie descrisa problema." << endl
+             << "Alegeti o optiune:" << endl
+             << "     1. Trimitere sesizare" << endl
+             << "     2. Inapoi" << endl
+             << "Optiune: ";
+        cin >> opt;
+        switch (opt)
+        {
+        case 1:
+            break;
+        case 2:
+
+            Meniu_Autentificat(fisier);
+            break;
+        default:
+            break;
+        }
+    }
+}
+int main()
+{
     Meniu_principal();
     return 0;
 }
